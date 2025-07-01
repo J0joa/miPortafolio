dc2761d629a6d7b0bdb0a491128ce4e4a0351e94
J0joa <4j0joa@gmail.com>
2025-06-30 21:58:20 -0500

    Agregué sección de contacto y mejoras al diseño

--- mi_Portafolio/vite.config.js
+++ mi_Portafolio/vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/miPortafolio/', // 👈 importante
-})
-
-
-
+  base: "https://J0joa.github.io/miPortafolio",
+});
